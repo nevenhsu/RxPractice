6baa1a457e6d509af33de801347ff22c77b8aaa5
nevenhsu <space.man0318@gmail.com>
2019-08-13 17:14:58 +0800

    Feat: UI

--- PhotoFilter/PhotoFilterVC.swift
+++ PhotoFilter/PhotoFilterVC.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotoFilterVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
 
