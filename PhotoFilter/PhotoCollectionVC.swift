7de2d75657331d3f96af39fbc2751a2825911b59
nevenhsu <space.man0318@gmail.com>
2019-08-14 16:31:31 +0800

    增加 observable

--- PhotoFilter/PhotoCollectionVC.swift
+++ PhotoFilter/PhotoCollectionVC.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import Photos
+import RxSwift
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "PhotoCollectionViewCell"
 
 class PhotoCollectionVC: UICollectionViewController {
+    
+    private var images = [PHAsset]()
+    
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObserver()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        populatePhoto()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        // self.collectionView!.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
     }
@@ -38,24 +50,46 @@ class PhotoCollectionVC: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return self.images.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PhotoCollectionViewCell else {
+            fatalError("No Photo cell")
+        }
+        
         // Configure the cell
+        
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { (image, _) in
+            DispatchQueue.main.async {
+                cell.photoImage?.image = image
+            }
+        }
     
         return cell
     }
 
     // MARK: UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let asset = self.images[indexPath.row]
+        
+        PHImageManager.default().requestImage(for: asset, targetSize: CGSize.init(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] (image, info) in
+            
+            // guard let info = info, image = image
+            
+        }
+    }
 
     /*
     // Uncomment this method to specify if the specified item should be highlighted during tracking
@@ -85,5 +119,27 @@ class PhotoCollectionVC: UICollectionViewController {
     
     }
     */
+    
+    private func populatePhoto () {
+        
+        PHPhotoLibrary.requestAuthorization { [weak self] (status) in
+            if (status == .authorized) {
+                // access photo library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                
+                assets.enumerateObjects({ (object, index, stop) in
+                    self?.images.append(object)
+                })
+                
+                self?.images.reverse()
+                
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+                
+            }
+        }
+        
+    }
 
 }
---
+++ PhotoFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoFilter
+//
+//  Created by Neven Hsu on 2019/8/14.
+//  Copyright © 2019 Neven Hsu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    
+}
